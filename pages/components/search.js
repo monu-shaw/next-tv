05eb93b385fd6ae612dc2040281027a6ab11cea0
Monu shaw <premraj00071@gmail.com>
2022-12-14 07:10:42 +0530

    Buil up

--- pages/components/search.js
+++ pages/components/search.js
@@ -7,9 +7,9 @@ function Search(props) {
     }
   return (
     <>
-    {props.display?(<i class="bi bi-search absolute top-0 right-0 p-2" onClick={()=>{props.setDisplay(!props.display)}}></i>):(<p className={`absolute top-0 right-0 p-2`} onClick={()=>{props.setDisplay(!props.display)}}>X</p>)}
-    <div className={`absolute top-0 w-full backdrop-blur-sm backdrop-sepia-4 flex justify-center transition-all ease-in duration-3000 ${props.display?'hidden h-0':'min-h-screen'}`}>
-    {props.display?(<i class="bi bi-search absolute top-0 right-0 p-2" onClick={()=>{props.setDisplay(!props.display)}}></i>):(<p className={`absolute top-0 right-0 p-2`} onClick={()=>{props.setDisplay(!props.display)}}>X</p>)}
+    {!props.display?(<i class="bi bi-search absolute top-0 right-0 p-2" onClick={()=>{props.setDisplay(!props.display)}}></i>):(<p className={`absolute top-0 right-0 p-2`} onClick={()=>{props.setDisplay(!props.display)}}>X</p>)}
+    <div className={`absolute top-0 w-full backdrop-blur-sm backdrop-sepia-4 flex justify-center transition-all ease-in duration-3000 ${!props.display?'hidden h-0':'min-h-screen'}`}>
+    {!props.display?(<i class="bi bi-search absolute top-0 right-0 p-2" onClick={()=>{props.setDisplay(!props.display)}}></i>):(<p className={`absolute top-0 right-0 p-2`} onClick={()=>{props.setDisplay(!props.display)}}>X</p>)}
         <div className={`p-2 backdrop-blur-lg  transition-all ease-in duration-3000 ${props.display?'hidden':'top-10'}`}>
             <input className='' placeholder='Channels' onChange={(e)=>setSearch(e.target.value)}/>
         </div>
