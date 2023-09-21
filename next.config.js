ae2b84cc97cf2279ffa2ccfa63378f4a8c4dad2d
Monu Shaw <68293997+monu-shaw@users.noreply.github.com>
2023-09-21 00:57:06 +0000

    ""

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    env:{
-      clientID: "https://hbimmaasrgaigjgznzao.supabase.co",
-      clientKEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiaW1tYWFzcmdhaWdqZ3puemFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA1ODc2NTQsImV4cCI6MTk4NjE2MzY1NH0.PTTRUyVPE-7pI7TanxamWfgcbcNqjeN97F2IaQYA0O8'
-    }
   }
   
 module.exports = nextConfig
